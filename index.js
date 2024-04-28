e7287f58e451d141e44b51973e00847b3e9195cc
1976udit <165473837+1976udit@users.noreply.github.com>
2024-04-28 18:51:56 +0000

    new

---
+++ index.js
@@ -0,0 +1,21 @@
+// import express from 'express'         both line 1 and 2 has same meaning
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req , res) => {
+     res.send("Happy Diwali");
+})
+
+app.get('/insta', (req,res) => {
+     res.send("This is insta page");
+})
+
+app.get('/login', (req , res) => {
+    res.send("<h1>This is Login Page</h1>")
+})
+
+app.listen(port , () => {
+    console.log(`Eample of port ${port}`)
+})
